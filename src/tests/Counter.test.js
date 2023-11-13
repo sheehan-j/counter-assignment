51d9a3ff619ea3bc8fdc5d687bb75b44a11ef1d9
sheehan-j <jordansheehan@ufl.edu>
2023-11-13 12:16:12 -0500

    Unit tests for Counter component

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,5 +1,8 @@
 // import necessary react testing library helpers here
 // import the Counter component here
+import {render, screen, fireEvent} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Counter from '../components/Counter';
 
 beforeEach(() => {
   // Render the Counter component here
@@ -7,16 +10,49 @@ beforeEach(() => {
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
+  expect(screen.getByText('Counter', {selector: 'h2'})).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
+  const initialValue = screen.getByTestId('count');
+  expect(initialValue).toHaveTextContent('0');
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
+  const counter = screen.getByTestId('count');
+  const initialCount = parseInt(counter.textContent, 10);
+  const incrementBtn = screen.getByText('+');
+
+  // Verify increment button displays
+  expect(incrementBtn).toBeInTheDocument();
+
+  // Click the button
+  fireEvent.click(incrementBtn);
+
+  // Verify the count is incremented
+  const expectedCount = (initialCount+1).toString()
+  expect(counter).toHaveTextContent(expectedCount);
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
+  const counter = screen.getByTestId('count');
+  const initialCount = parseInt(counter.textContent, 10);
+  const decrementBtn = screen.getByText('-');
+
+  // Verify increment button displays
+  expect(decrementBtn).toBeInTheDocument();
+
+  // Click the button
+  fireEvent.click(decrementBtn);
+
+  // Verify the count is incremented
+  const expectedCount = (initialCount-1).toString()
+  expect(counter).toHaveTextContent(expectedCount);
 });
